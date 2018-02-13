a0073d8485be5646964d9da1eaa8b6dcb5f6e1bf
Bhaskar Gautam <bhaskar24@users.noreply.github.com>
2018-02-13 07:55:57 +0530

    Switch to use python-tweepy (#6)
    * Delete config.py.example
    
    * Resolved bugs using twitter api
    
    * Updated setup
    
    * Update README.md

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # IBM Watson Personality Insights Twitter Python Sample
 
 This sample shows how to get Twitter data using the [Twitter REST API](https://dev.twitter.com/rest/public) 
-(via the [python-twitter client library](https://github.com/bear/python-twitter)) and submit it to the 
+(via the [python-tweepy client library](https://github.com/tweepy/tweepy)) and submit it to the 
 [Personality Insights Service](https://www.ibm.com/smarterplanet/us/en/ibmwatson/developercloud/personality-insights.html).
 
 For non-twitter samples and more details on how to setup your Personality Insights service in bluemix see the [official 
@@ -9,16 +9,14 @@ Watson Developer Cloud samples](https://github.com/watson-developer-cloud).
 
 ## Setup
 
-This sample was developed on Python 2.6.6 with the `python-twitter` and `requests` libraries, installed via pip.
+This sample was developed on Python 2.7.2 with the `python-tweepy` and `requests` libraries, installed via pip.
 
 ## Configuring your Twitter and Personality Insights Credentials
 
-To configure the sample, copy `config.py.example` to `config.py` 
-and edit `config.py` to fill in your Twitter and Personality Insights Credentials. Instructions are provided in the `config.py.example` file.
+To configure the sample, edit `twitteranalyzer.py` to fill in your Twitter and Personality Insights Credentials. Instructions are provided in the `comments` in same file.
 
 ## Running the sample
 
 Provide a twitter handle/screen name WITHOUT the leading @ sign.  Example:
 
     python twitteranalyzer.py jschoudt
-
--- twitteranalyzer.py
+++ twitteranalyzer.py
@@ -1,9 +1,10 @@
 import sys
 import requests
 import json
-import twitter
+import tweepy
 import config
-
+import time
+import calendar
 
 def convert_status_to_pi_content_item(s):
     # My code here
@@ -14,7 +15,6 @@ def convert_status_to_pi_content_item(s):
         'contenttype': 'text/plain',
         'language': s.lang,
         'content': s.text,
-        'created': s.created_at_in_seconds,
         'reply': (s.in_reply_to_status_id == None),
         'forward': False
     }
@@ -22,36 +22,68 @@ def convert_status_to_pi_content_item(s):
 
 handle = sys.argv[1]
 
-twitter_api = twitter.Api(consumer_key=config.twitter_consumer_key,
-                          consumer_secret=config.twitter_consumer_secret,
-                          access_token_key=config.twitter_access_token,
-                          access_token_secret=config.twitter_access_secret,
-                          debugHTTP=True)
+# Twitter Credentials
+#
+# To obtain the credentials, you must first have a Twitter account. 
+# Then, go to https://dev.twitter.com/, login, and click on "Manage Your Apps" to reach https://apps.twitter.com/.
+# Then, click "Create New App", fill in the relevant fields, and click "Create your Twitter application".
+# With the application created, navigate to the API Keys page and click "Create my access token". 
+# You now have the four necessary credentials. Copy the API key, API secret, 
+# Access token, and Access token secret here.
+# NOTE: API key and API secret go in the twitter_consumer_key and twitter_consumer_secret vars.
+#
+
+access_key=""
+access_secret=""
+consumer_key=""
+consumer_secret=""
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_key, access_secret)
+
+twitter_api = tweepy.API(auth)
 
 max_id = None
 statuses = []
+
 for x in range(0, 16):  # Pulls max number of tweets from an account
     if x == 0:
-        statuses_portion = twitter_api.GetUserTimeline(screen_name=handle,
+        statuses_portion = twitter_api.user_timeline(screen_name=handle,
                                                        count=200,
                                                        include_rts=False)
         status_count = len(statuses_portion)
         max_id = statuses_portion[status_count - 1].id - 1  # get id of last tweet and bump below for next tweet set
     else:
-        statuses_portion = twitter_api.GetUserTimeline(screen_name=handle,
+        statuses_portion = twitter_api.user_timeline(screen_name=handle,
                                                        count=200,
                                                        max_id=max_id,
                                                        include_rts=False)
         status_count = len(statuses_portion)
-        max_id = statuses_portion[status_count - 1].id - 1  # get id of last tweet and bump below for next tweet set
+        print status_count
+        try:
+            max_id = statuses_portion[status_count - 1].id - 1  # get id of last tweet and bump below for next tweet set
+        except:
+            pass
     for status in statuses_portion:
         statuses.append(status)
+
+print 'Number of Tweets user have: '+str(len(statuses))
 
 pi_content_items_array = map(convert_status_to_pi_content_item, statuses)
 pi_content_items = {'contentItems': pi_content_items_array}
 
-r = requests.post(config.pi_url + '/v2/profile',
-                  auth=(config.pi_username, config.pi_password),
+# Personality Insights credentials and URL
+#
+# You can obtain these credentials by binding a PI service to an application in bluemix and 
+# and clicking the "show credentials" link on the service in the application dashboard.
+# Or you can use "cf env <application name>" from the command line to get the credentials.
+
+pi_url = ''
+pi_username = ''
+pi_password = ''
+
+r = requests.post(pi_url + '/v2/profile',
+                  auth=(pi_username, pi_password),
                   headers={
                       'content-type': 'application/json',
                       'accept': 'application/json'
